619c1b64e0d17a02393d5390029ac6a5f445a6d3
DesertPixelAi <158174152+DesertPixelAi@users.noreply.github.com>
2025-02-07 11:58:10 +0200

    Add files via upload

---
+++ js/dp_line_cycler.js
@@ -0,0 +1,26 @@
+import { app } from "../../scripts/app.js";
+
+app.registerExtension({
+    name: "dp.line.cycler",
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        if (nodeData.name === "DP Line Cycler") {
+            const onNodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                const result = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+                
+                // Handle WebSocket messages
+                api.addEventListener("update_node", ({ detail }) => {
+                    if (detail.node_id === this.id) {
+                        const widget = this.widgets.find(w => w.name === "index");
+                        if (widget) {
+                            widget.value = detail.index_value;
+                            widget.callback(widget.value);
+                        }
+                    }
+                });
+                
+                return result;
+            };
+        }
+    }
+});
